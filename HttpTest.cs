2771ee2c8b5475ad5089830f940f47d43d80022f
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2024-01-21 21:34:08 +0100

    234

--- HttpTest.cs
+++ HttpTest.cs
@@ -22,7 +22,7 @@ namespace DotNetIsolatedTest
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("123");
+            response.WriteString("234");
 
             return response;
         }
